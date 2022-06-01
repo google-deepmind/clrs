3ba3f2d5f7e832d8076b5bb9c8c05d3dfc4bf18e
CLRSDev <clrs-dev@google.com>
2022-06-01 07:29:23 -0700

    Increase version number for PyPI release.
    PiperOrigin-RevId: 452292156

--- clrs/__init__.py
+++ clrs/__init__.py
@@ -42,7 +42,7 @@ from clrs._src.specs import SPECS
 from clrs._src.specs import Stage
 from clrs._src.specs import Type
 
-__version__ = "0.0.2"
+__version__ = "1.0.0"
 
 __all__ = (
     "build_sampler",
